170ef5035bd23909ba1e73eb0e42cdd51bd1e7d9
POLITJ <jospolit@gmail.com>
2019-07-09 11:25:46 +0200

    1 to 9

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, c'est quoi ton prenom ?"
+print "> "
+user_name = gets.chomp
+puts "C'est quoi ton nom de famille?"
+print "> "
+user_surname = gets.chomp
+puts "Bonjour " + user_name + " " + user_surname + "!"
